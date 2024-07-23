9a72d8797279c3ea7f075dc74bcf1a43ee99b183
Ankit Anand <ankitanand9891@gmail.com>
2024-07-23 12:04:18 +0530

    third commit

--- firstQuestion.py
+++ firstQuestion.py
@@ -6,7 +6,7 @@
 #         print(f"{i} is odd")
 """ Given a list of integers, write a Python script that uses a for loop and if-else statements to print each number followed by "is even" if the number is even, and "is odd"
 if the number is odd """
-numList=[1,3,7,9,12,10,17,45]  
+numList=[1,3,7,9,12,10,17,45,78]  
 for i in numList:
     if i%2 ==0:
         print(f"{i} is even")
